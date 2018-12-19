c305db81e90c20937baffa12074236626f04307a
Adam Zamlynny <azamlynny@hotmail.com>
2018-12-18 19:49:59 -0500

    Added basic teleop control

--- research-2019/src/main/java/frc/robot/Robot.java
+++ research-2019/src/main/java/frc/robot/Robot.java
@@ -1,35 +1,56 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-package frc.robot;
-
+import edu.wpi.cscore.AxisCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends IterativeRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
 
-  @Override
-  public void robotInit() {
-    m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
-  }
+    WPI_TalonSRX FrontRight;
+    WPI_TalonSRX FrontLeft;
+    WPI_TalonSRX BackRight;
+    WPI_TalonSRX BackLeft;
+    SpeedControllerGroup Right;
+    SpeedControllerGroup Left;
+    DifferentialDrive DriveTrain;
+    XboxController Controller;
 
-  @Override
-  public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
-  }
+    void RobotInit {
+        FrontRight = new WPI_TalonSRX(2);
+        FrontLeft = new WPI_TalonSRX(3);
+        BackRight = new WPI_TalonSRX(4);
+        BackLeft = new WPI_TalonSRX(5);
+
+        Right = new SpeedControllerGroup(FrontRight, BackRight);
+        Left = new SpeedControllerGroup(FrontLeft, BackLeft);
+
+        DriveTrain = new DifferentrialDrive(Left, Right);
+
+        Controller = new XboxController(1);
+    }
+
+    void teleopInit {
+        
+    }
+
+    void teleopPeriodic {
+        
+        if(Controller.getAButton()) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kLeft);
+            DriveTrain.arcadeDrive(y, x);
+        } else if(Controller.getBumper(GenericHID.Hand.kLeft)) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kRight);
+            DriveTrain.arcadeDrive(y, x);
+        } else if(Controller.getBumper(GenericHID.Hand.kRight)) {
+            double y = Controller.getX(GenericHID.Hand.kRight);
+            double x = Controller.getY(GenericHID.Hand.kLeft);
+            DriveTrain.arcadeDrive(y, x);
+        } 
+    }
 }
