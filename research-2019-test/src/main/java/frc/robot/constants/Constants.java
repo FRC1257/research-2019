8e0b777a278a56231c9756562a92d39ef727f093
Adam Zamlynny <azamlynny@hotmail.com>
2019-01-07 17:22:59 -0500

    Removed old wpilib

--- research-2019-test/src/main/java/frc/robot/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.constants;
-
-public class Constants{
-    public static final double cameraHeight = 8; // placeholder
-    public static final double cameraAngle = 0;
-    
-    public static final double target1_height = 48;
-    public static final double target1_distance = 36;
-}
