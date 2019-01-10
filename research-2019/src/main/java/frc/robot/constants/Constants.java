3490f2372b6bc529da1c722476ee8b1e7f593521
Adam Zamlynny <azamlynny@hotmail.com>
2019-01-10 15:52:54 -0500

    Added estimating distance based on the target area constants saved.

--- research-2019/src/main/java/frc/robot/constants/Constants.java
+++ research-2019/src/main/java/frc/robot/constants/Constants.java
@@ -7,5 +7,6 @@ public class Constants{
     public static final double target1_height = 48;
     public static final double target1_distance = 96;
 
+    public static final int measurementAmount = 120;
     public static final double[] distanceToPercent = {};
 }
--- research-2019/src/main/java/frc/robot/vision/Vision.java
+++ research-2019/src/main/java/frc/robot/vision/Vision.java
@@ -60,6 +60,20 @@ public class Vision{
         return((Constants.target1_height - Constants.cameraHeight) / Math.toDegrees(Math.tan(Constants.cameraAngle + tyE.getDouble(0)))); // Use trig knowing the height of the object and angle to find distnace
     } 
     
+    public static double tableDistanceFromObject(NetworkTable table){
+        NetworkTableEntry taE = table.getEntry("ta");
+        double ta = taE.getDouble(0);
+        double minDifference = 10000;
+        int minIndex = 119;
+        for(int i = 0; i < Constants.measurementAmount - 1; i++){
+            if(Constants.distanceToPercent[i] - ta < minDifference){
+                minDifference = Constants.distanceToPercent[i] - ta;
+                minIndex = 1;
+            }
+        }
+        return(Constants.distanceToPercent[minIndex]);
+    }
+
     public static void findCameraAngle(NetworkTable table, double distance){ // a1 = arctan((h2 - h1)/d) - a2
         //One time function used to determine the angle at which the camera is mounted at
         //After the angle is found, it is put in the code permanently in Constants.java and never edited. (unless the limelight is moved physically on the robot)
