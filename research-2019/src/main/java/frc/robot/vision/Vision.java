c5d2f2704480fc50b62ad37275b6148aa1a7bd1c
Adam Zamlynny <azamlynny@hotmail.com>
2019-02-08 23:26:41 -0500

    Cleaned up code a lot and removed the gyro antidrift

--- research-2019/src/main/java/frc/robot/vision/Vision.java
+++ research-2019/src/main/java/frc/robot/vision/Vision.java
@@ -44,23 +44,6 @@ public class Vision{
         return(driving_adjust);
     }
 
-    // public static void findObject(NetworkTable table, DifferentialDrive DriveTrain){ // Spins until the robot finds the target
-    //     NetworkTableEntry tvE = table.getEntry("tv");
-    //     double tv = tvE.getDouble(0);
-    //     if(tv == 0.0){ // No target on screen, then spin
-    //         DriveTrain.arcadeDrive(0, 1); 
-    //     }
-    //     else if(tv == 1.0){ // Target on screen, then aim on it
-    //         // angleCorrect(table,);
-    //     }
-    // }
-
-   //trig method, not viable for 2019 since target is low 
-    public static double distanceFromObject(NetworkTable table){ //d = (h2-h1) / tan(a1+a2)
-        NetworkTableEntry tyE = table.getEntry("ty");
-        return((Constants.target1_height - Constants.cameraHeight) / Math.toDegrees(Math.tan(Constants.cameraAngle + tyE.getDouble(0)))); // Use trig knowing the height of the object and angle to find distnace
-    } 
-    
     public static double tableDistanceFromObject(NetworkTable table){
         NetworkTableEntry taE = table.getEntry("ta");
         NetworkTableEntry tvE = table.getEntry("tv");
@@ -77,30 +60,11 @@ public class Vision{
                     minIndex = i;
                 }
             }
-            
             return(minIndex);
         }
         else{
-            return(0); 
+            return(0); // If there is no target on the screen then do not drive 
         }
     }
 
-    public static void findCameraAngle(NetworkTable table, double distance){ // a1 = arctan((h2 - h1)/d) - a2
-        //One time function used to determine the angle at which the camera is mounted at
-        //After the angle is found, it is put in the code permanently in Constants.java and never edited. (unless the limelight is moved physically on the robot)
-        //The robot must be put a distance away from the target that is KNOWN and is EXACT
-        NetworkTableEntry tyE = table.getEntry("ty");
-        double cameraAngle = Math.toDegrees(Math.atan((Constants.target1_height - Constants.cameraHeight)) / distance) - tyE.getDouble(0);
-        System.out.println(cameraAngle); // Not sure if works, shuffleboard could be used
-        SmartDashboard.putNumber("Elevator Height ", cameraAngle);
-    }
-
-    //Handles all vision firing and other stuff
-    // public static void shoot(NetworkTable table, DifferentialDrive DriveTrain){ //Sample code that the robot fired the projectile
-        // DriveTrain.arcadeDrive(getInDistance(table), angleCorrect(table)); // getInDistance and angleCorrect return values to correct the robot.
-        // table.getEntry("snapshot").set(1);
-        // 
-    // }
-
-    
 }
