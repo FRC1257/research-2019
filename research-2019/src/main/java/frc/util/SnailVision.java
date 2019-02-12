265df884f14ffd6222bbeb492e697e318979b5fa
Adam Zamlynny <azamlynny@hotmail.com>
2019-02-11 23:19:36 -0500

    Added basic backbone

--- research-2019/src/main/java/frc/util/SnailVision.java
+++ research-2019/src/main/java/frc/util/SnailVision.java
@@ -5,4 +5,36 @@ public class SnailVision {
     public SnailVision(){
     }
 
+    public static void visionFunctionality(){
+
+    }
+
+    public static double angleCorrect(){
+
+        return(0); // temp
+    }
+
+    public static double getInDistance(){
+
+        return(0); // temp
+    }
+    
+    public static double areaDistance(){
+
+        return(0); // temp
+    }
+
+    public static double trigDistance(){
+
+        return(0); // temp
+    }
+
+    public static double findTarget(){
+
+        return(0); // temp
+    }
+
+    public static void takeScreenshot(){
+        
+    }
 }
