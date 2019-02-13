b00f144a9f932a5e3fe2699ae591dce27e777a5a
Adam Zamlynny <azamlynny@hotmail.com>
2019-02-12 20:41:03 -0500

    Addded a lot of functions

--- research-2019/src/main/java/frc/util/snail_vision/SnailVision.java
+++ research-2019/src/main/java/frc/util/snail_vision/SnailVision.java
@@ -52,7 +52,10 @@ public class SnailVision {
     }
 
     public static void changePipeline(NetworkTable Table, int pipeline){
-
+        if(pipeline < 0 || pipeline > 9){
+            pipeline = 0; // In case that someone tries to switch to an impossible pipeline then set it to default.
+        }
+        Table.getEntry("pipeline").setNumber(pipeline);
     }
 
     public static void toggleScreenshot(NetworkTable Table){ // Takes 2 screenshots per second
@@ -61,6 +64,33 @@ public class SnailVision {
         }
         else if(Table.getEntry("snapshot").getDouble(0) == 1){ // If the camera is taking screenshots currently
             Table.getEntry("snapshot").setNumber(0);
+        }
+    }
+
+    public static void turnOffLimelight(NetworkTable Table){
+        if(Table.getEntry("ledMode").getDouble(0) != 1){
+            Table.getEntry("ledMode").setNumber(1);
+        }
+    }
+
+    public static void turnOnLimelight(NetworkTable Table){
+        if(Table.getEntry("ledMode").getDouble(0) != 3){
+            Table.getEntry("ledMode").setNumber(3);
+        }
+    }
+
+    public static void blinkLimelight(NetworkTable Table){
+        if(Table.getEntry("ledMode").getDouble(0) != 2){
+            Table.getEntry("ledMode").setNumber(2);
+        }
+    }
+
+    public static void toggleCameraMode(NetworkTable Table){ // 0 = vision processing 1 = drive camera
+        if(Table.getEntry("camMode").getDouble(0) == 0){
+            Table.getEntry("camMode").setNumber(1);
+        }
+        else if(Table.getEntry("camMode").getDouble(0) == 1){
+            Table.getEntry("camMode").setNumber(0);
         }
     }
 
