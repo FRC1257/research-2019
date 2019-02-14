e422dd5d5dcb2af20e664e294d86062d07442622
Adam Zamlynny <azamlynny@hotmail.com>
2019-02-14 16:33:22 -0500

    Added a lot of variables that will be used and limelight input functionality

--- research-2019/src/main/java/frc/util/snail_vision/SnailVision.java
+++ research-2019/src/main/java/frc/util/snail_vision/SnailVision.java
@@ -15,14 +15,75 @@ public class SnailVision {
     double CAMERA_HEIGHT;
     double CAMERA_ANGLE;
 
+    ArrayList<Double> TargetX; // Target's angle on the x-axis 
+    ArrayList<Double> TargetY; // Target's angle on the y-axis 
+    ArrayList<Double> TargetA; // Target's area on the screen
+    ArrayList<Double> TargetV; // Target's Visibility on the screen 1.0 = true 0.0 = false
+    ArrayList<Double> TargetS; // Target's skew/rotation on the screen
+    ArrayList<Double> Latency; // Latency of the camera in miliseconds
+    ArrayList<Double> TargetShort; // Sidelength of shortest side of the fitted bounding box (pixels)
+    ArrayList<Double> TargetLong; // Sidelength of longest side of the fitted bounding box (pixels)
+    ArrayList<Double> TargetHorizontal; // Horizontal length of the fitted bounding box
+    ArrayList<Double> TargetVertical; // Vertical length of the fitted bounding box
+    ArrayList<Double> currentPipeline; // Array because it might be used when switching pipeline
+
     ArrayList<Double> Targets = new ArrayList<Double>();
 
     public SnailVision(){
-
+        TargetX = new ArrayList<Double>(); // Target's angle on the x-axis 
+        TargetY = new ArrayList<Double>(); // Target's angle on the y-axis 
+        TargetA = new ArrayList<Double>(); // Target's area on the screen
+        TargetV = new ArrayList<Double>(); // Target's visibility on the screen 1.0 = true 0.0 = false
+        TargetS = new ArrayList<Double>(); // Target's skew/rotation on the screen
+        Latency = new ArrayList<Double>(); // Latency of the camera in miliseconds
+        TargetShort = new ArrayList<Double>(); // Sidelength of shortest side of the fitted bounding box (pixels)
+        TargetLong = new ArrayList<Double>(); // Sidelength of longest side of the fitted bounding box (pixels)
+        TargetHorizontal = new ArrayList<Double>(); // Horizontal length of the fitted bounding box
+        TargetVertical = new ArrayList<Double>(); // Vertical length of the fitted bounding box
+        currentPipeline = new ArrayList<Double>(); // Array because it might be used when switching pipeline
     }
 
-    public static void visionFunctionality(NetworkTable Table){
+    public static void limelightFunctionality(){
+        NetworkTable Table = NetworkTableInstance.getDefault().getTable("limelight");
         
+        // Creates objects which retrieve data from the limelight
+        NetworkTableEntry txE = Table.getEntry("tx");
+        NetworkTableEntry tyE = Table.getEntry("ty");
+        NetworkTableEntry taE = Table.getEntry("ta");
+        NetworkTableEntry tvE = Table.getEntry("tv");
+        NetworkTableEntry tsE = Table.getEntry("ts");
+        NetworkTableEntry tlE = Table.getEntry("tl");
+        NetworkTableEntry tshortE = Table.getEntry("tshort");
+        NetworkTableEntry tlongE = Table.getEntry("tlong");
+        NetworkTableEntry thorE = Table.getEntry("thor");
+        NetworkTableEntry tverE = Table.getEntry("tvert");
+        NetworkTableEntry tgetpipeE = Table.getEntry("getpipe");
+
+        TargetX.add(0, txE.getDouble(0)); // Target's angle on the x-axis 
+        TargetY.add(0, tyE.getDouble(0)); // Target's angle on the y-axis 
+        TargetA.add(0, taE.getDouble(0)); // Target's area on the screen
+        TargetV.add(0, tvE.getDouble(0)); // Target's visibility on the screen 1.0 = true 0.0 = false
+        TargetS.add(0, tsE.getDouble(0)); // Target's skew/rotation on the screen
+        Latency.add(0, tlE.getDouble(0)); // Latency of the camera in miliseconds
+        TargetShort.add(0, tshortE.getDouble(0)); // Sidelength of shortest side of the fitted bounding box (pixels)
+        TargetLong.add(0, tlongE.getDouble(0)); // Sidelength of longest side of the fitted bounding box (pixels)
+        TargetHorizontal.add(0, thorE.getDouble(0)); // Horizontal length of the fitted bounding box
+        TargetVertical.add(0, tvertE.getDouble(0)); // Vertical length of the fitted bounding box
+        currentPipeline.add(0, tgetpipeE.getDouble(0));
+            
+        if(TargetX.size > 60){ // Removes the last entry in the arraylist and shifts over the rest
+            TargetX.remove(60); // Target's angle on the x-axis 
+            TargetY.remove(60); // Target's angle on the y-axis 
+            TargetA.remove(60); // Target's area on the screen
+            TargetV.remove(60); // Target's visibility on the screen 1.0 = true 0.0 = false
+            TargetS.remove(60); // Target's skew/rotation on the screen
+            Latency.remove(60); // Latency of the camera in miliseconds
+            TargetShort.remove(60); // Sidelength of shortest side of the fitted bounding box (pixels)
+            TargetLong.remove(60); // Sidelength of longest side of the fitted bounding box (pixels)
+            TargetHorizontal.remove(60); // Horizontal length of the fitted bounding box
+            TargetVertical.remove(60); // Vertical length of the fitted bounding box
+            currentPipeline.remove(60); // Array because it might be used when switching pipeline
+        }
     }
 
     public static double angleCorrect(NetworkTable Table){
@@ -85,7 +146,7 @@ public class SnailVision {
         }
     }
 
-    public static void toggleCameraMode(NetworkTable Table){ // 0 = vision processing 1 = drive camera
+    public static void toggleLimelightMode(NetworkTable Table){ // 0 = vision processing 1 = drive camera
         if(Table.getEntry("camMode").getDouble(0) == 0){
             Table.getEntry("camMode").setNumber(1);
         }
