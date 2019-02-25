37e6771693bbf5224ef609aac93948c421603f2f
Adam Zamlynny <azamlynny@hotmail.com>
2019-02-25 16:37:43 -0500

    Added controls, driving, and autoturning

--- research-2019/src/main/java/frc/robot/Robot.java
+++ research-2019/src/main/java/frc/robot/Robot.java
@@ -13,17 +13,50 @@ import edu.wpi.first.networktables.*;
 import java.util.ArrayList;
 import com.kauailabs.navx.frc.*;
 import frc.util.snail_vision.*;
+import edu.wpi.first.wpilibj.drive.*;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.networktables.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.I2C.Port;
 
 public class Robot extends TimedRobot {
 
+    WPI_TalonSRX FrontRight;
+    WPI_TalonSRX FrontLeft;
+    WPI_TalonSRX BackRight;
+    WPI_TalonSRX BackLeft;
+    SpeedControllerGroup Right;
+    SpeedControllerGroup Left;
+    DifferentialDrive DriveTrain;
+    XboxController Controller;
+    ArrayList<Double> DistanceToArea;
+    Boolean leftStickPressed;
+    Boolean rightStickPressed;
+    double driveSpeed;
+    double turnSpeed;
+    
     SnailVision vision;
-    public static final double[] AREA_TO_DISTANCE_ROCKET;
-    public static final double[] AREA_TO_DISTANCE_SHIP;
+    public static final double[] AREA_TO_DISTANCE_ROCKET = {1};
+    public static final double[] AREA_TO_DISTANCE_SHIP = {1};
 
     @Override
     public void robotInit () {
+
+        FrontLeft = new WPI_TalonSRX(3);
+        BackLeft = new WPI_TalonSRX(6);
+        BackRight = new WPI_TalonSRX(1);
+        FrontRight = new WPI_TalonSRX(2);
+
+        Right = new SpeedControllerGroup(FrontRight, BackRight);
+        Left = new SpeedControllerGroup(FrontLeft, BackLeft);
+
+        DriveTrain = new DifferentialDrive(Left, Right);
+
+        Controller = new XboxController(0);
+
+        driveSpeed = 0;
+        turnSpeed = 0;
 
         vision = new SnailVision(false);
 
@@ -51,6 +84,35 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopPeriodic () {
         // NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        vision.networkTableFunctionality(NetworkTableInstance.getDefault().getTable("limelight"));
+
+        driveSpeed = 0;
+        turnSpeed = 0;
+
+         // Basic Teleop Drive Code
+         if(Controller.getAButton()) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kLeft);
+            driveSpeed = -y;
+            turnSpeed = x;
+        } 
+        else if(Controller.getBumper(GenericHID.Hand.kLeft)) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kRight);
+            driveSpeed = -y;
+            turnSpeed = x;
+        } 
+        else if(Controller.getBumper(GenericHID.Hand.kRight)) {
+            double x = Controller.getX(GenericHID.Hand.kLeft);
+            double y = Controller.getY(GenericHID.Hand.kRight);
+            driveSpeed = -y;
+            turnSpeed = x;
+        }
+
+        if(Controller.getTriggerAxis(GenericHID.Hand.kLeft) > 0){ //If left trigger pressed and a target on screen then turn to it
+            
+            turnSpeed += vision.angleCorrect();
+        }
     }
  
 }
